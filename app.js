55b74753ad58f3a4ca2a2ed85ecb92999b049b0b
Lucidious45 <vandenhoofy@gmail.com>
2021-11-14 21:35:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class Stopwatch{
     constructor(){
-        this.time;
-        this.runningTime = 0;
+        var hr = 0;
+        var min = 0;
+        var sec = 0;
+        var stoptime = true;
+        
 
 /*         this.status = "paused";
  */    }
@@ -15,14 +18,13 @@ class Stopwatch{
             console.log("A timer has already been started.")
         }
  */
-            if (!this.time) {
-            this.time = setInterval(this.runningTime, 1000);
-        } else {
-            console.log("A timer has already been started.")
+        if (stoptime == true) {
+            stoptime = false;
+            timerCycle();
         }
     }
 
-    start2 () {
+    /* start2 () {
                 if (!this.time) {
                     this.time = setInterval(this.test, 1000);
                 } else {
@@ -32,11 +34,46 @@ class Stopwatch{
 
     test(){
         return console.log("timer werkt")
-    }
+    } */
 
     end (){
-        this.time = clearInterval(this.time)
-        console.log("Your timer has been set back to 0")
+        if (stoptime == false) {
+            stoptime = true;
+          }
+    }
+
+    timerCycle() {
+        if (stoptime == false) {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+    
+        sec = sec + 1;
+    
+        if (sec == 60) {
+          min = min + 1;
+          sec = 0;
+        }
+        if (min == 60) {
+          hr = hr + 1;
+          min = 0;
+          sec = 0;
+        }
+    
+        if (sec < 10 || sec == 0) {
+          sec = '0' + sec;
+        }
+        if (min < 10 || min == 0) {
+          min = '0' + min;
+        }
+        if (hr < 10 || hr == 0) {
+          hr = '0' + hr;
+        }
+    
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+    
+        setTimeout("timerCycle()", 1000);
+      }
     }
 
     reset() {
